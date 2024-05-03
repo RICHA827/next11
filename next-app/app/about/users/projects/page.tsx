de49cbe1606ed21fa8c042f7c9ece3d4c5614c1f
Richa Punera <puneraricha@gmail.com>
2024-05-03 18:19:32 +0530

    feature signup working

---
+++ next-app/app/about/users/projects/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Projects = () => {
+  return (
+    <div>User projects are available here </div>
+  )
+}
+
+export default Projects
---
+++ next-app/app/portfolio/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Portfolio = () => {
+  return (
+    <div>This is my complete Portfolio</div>
+  )
+}
+
+export default Portfolio
